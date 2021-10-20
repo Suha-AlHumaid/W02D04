b320151a7586af1499f31b6abfd29f1e2ab2b869
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-20 09:41:58 +0300

    practic

---
+++ main.js
@@ -0,0 +1,21 @@
+//1,2
+const bodyy = document.querySelector("body"); //to select the body
+const header1 = document.createElement("h1"); //create h1 (header)
+header1.innerHTML =" Todo List"; //add content in h1
+const uList = document.createElement("ol"); // create an unorder list
+uList.id = "uList";
+
+//3
+
+const toDos = ["wake up","eat breakfast", "code"]
+
+//4
+const randerList = () => {
+toDos.forEach (elem => {
+
+
+
+});
+
+
+}
