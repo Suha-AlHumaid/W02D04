dc87974e961d5db6527634fe7b56a66fd609b840
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-20 16:51:24 +0300

    del

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //1,2
 const body = document.querySelector("body"); //to select the body
 const header1 = document.createElement("h1"); //create h1 (header)
-header1.innerHTML = " Todo List"; //add content in h1
+header1.innerHTML = " To Do List"; //add content in h1
 const uList = document.createElement("ul"); // create an unorder list
 uList.id = "uList";
 
@@ -12,28 +12,40 @@ const toDos = ["wake up", "eat breakfast", "code"];
 
 //3--4
 const randerList = () => {
+    //Display list
   const liElements = document.querySelector("#uList");
   liElements.innerHTML = "";
   toDos.forEach((elem,i) => {
     const liList = document.createElement("li");
+    liList.id = i;
     liList.innerHTML = elem;
     liElements.append(liList);
+
     //delete btns
-    const deleteBtn = document.createElement("button")
-    deleteBtn.innerHTML="Delete";
-    deleteBtn.id=i;
+    // const deleteBtn = document.createElement("button")
+    // deleteBtn.innerHTML="Delete";
+    const deleteBtn = document.createElement("INPUT");
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.setAttribute("value", "Delete");
+    deleteBtn.id="delBtn";
+    deleteBtn.addEventListener("click",(i) => del(i));
     liList.append(deleteBtn);
+    // delBtn.addEventListener("click", () =>dele (i));
+   
+    
+    
     //edit btns
-    const editBtn = document.createElement("button")
-    editBtn.innerHTML="Edit";
-    editBtn.id=i;
+    // const editBtn = document.createElement("button")
+    // editBtn.innerHTML="Edit";
+    const editBtn = document.createElement("INPUT");
+    editBtn.setAttribute("type", "button");
+    editBtn.setAttribute("value", "Edit");
+    editBtn.id="editBtn";
     liList.append(editBtn);
+
   });
-  // for (let i =0 ;i<toDos.length;i++ ) {
-  //     const liList = document.createElement("li");
-  //     liList.innerHTML= toDos[i];
-  //     liElements.append(liList);
-  // }
+  
+
 };
 randerList();
 
@@ -52,14 +64,22 @@ body.append(text1);
 const btn1 = document.createElement("INPUT");
 btn1.setAttribute("type", "button");
 btn1.setAttribute("value", "Add");
-
 body.append(btn1);
 btn1.addEventListener("click", addToList);
 
 
 //6 Delete
+function del (index) {
+toDos.splice(index,1);
+randerList();
+  };
 
 
 
 //7
+  
+  
+const editList = () => {
 
+    randerList();
+  };
