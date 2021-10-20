d69da8a6d410280c7f025eee617763e335cde430
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-20 09:52:28 +0300

    append

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //1,2
-const bodyy = document.querySelector("body"); //to select the body
+const body = document.querySelector("body"); //to select the body
 const header1 = document.createElement("h1"); //create h1 (header)
 header1.innerHTML =" Todo List"; //add content in h1
 const uList = document.createElement("ol"); // create an unorder list
 uList.id = "uList";
+
+
+body.append(header1);
+body.append(uList);
 
 //3
 
@@ -11,6 +15,7 @@ const toDos = ["wake up","eat breakfast", "code"]
 
 //4
 const randerList = () => {
+
 toDos.forEach (elem => {
 
 
