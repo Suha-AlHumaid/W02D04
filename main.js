e68d73903896ecc30dbac5169becb8cb68968b49
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-10-20 10:16:17 +0300

    update

--- main.js
+++ main.js
@@ -2,25 +2,36 @@
 const body = document.querySelector("body"); //to select the body
 const header1 = document.createElement("h1"); //create h1 (header)
 header1.innerHTML =" Todo List"; //add content in h1
-const uList = document.createElement("ol"); // create an unorder list
+const uList = document.createElement("ul"); // create an unorder list
 uList.id = "uList";
 
 
-body.append(header1);
-body.append(uList);
+body.append(header1); //to append in body parent.append(element)
+body.append(uList); 
 
 //3
 
-const toDos = ["wake up","eat breakfast", "code"]
+const toDos = ["wake up","eat breakfast", "code"];
+
+
 
 //4
 const randerList = () => {
-
-toDos.forEach (elem => {
-
-
-
+const liElements = document.querySelector("#uList");
+// for (let i =0 ;i<toDos.length;i++ ) {
+//     const liList = document.createElement("li");
+//     liList.innerHTML= toDos[i];
+//     liElements.append(liList);
+// }
+toDos.forEach((elem) => {
+console.log(elem);
+const liList = document.createElement("li");
+liList.innerHTML= elem;
+liElements.append(liList);
 });
-
-
 }
+
+randerList();
+
+
+
